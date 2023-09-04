7709f8bea9c067c3e9abbae02d642ee6fbca9e64
DylanTartarini1996 <tartarinidylan@gmail.com>
2023-09-04 21:55:58 +0200

    UPDT

--- epm/setup.py
+++ epm/setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(name='epm',
       version='0.0',
       description='Energy Prices Monitoring & Forecasting',
-      author='Dylan Tartarini',
-      author_email='tartarinidylan@gmail.com',
+      author='Dylan Tartarini, Martina Lovat',
+      author_email='tartarinidylan@gmail.com', 'martinalovat96@gmail.com',
       url='https://github.com/DylanTartarini1996/energy_prices_monitoring_ita',
       packages=['distutils', 'distutils.command'],
      )
